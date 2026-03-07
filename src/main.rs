245def8fa7a5602d57635d0eae1df21cd503f5d4
mohamed hisham mahrous <hishamsovereign321@gmail.com>
2026-03-07 14:44:15 +0200

    add: actix middleware to log requests details

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use env_logger::Env;
 use sqlx::PgPool;
 use std::net::TcpListener;
 use zero2prod::configuration::get_configuration;
@@ -6,6 +7,8 @@ use zero2prod::startup::run;
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
     tracing_subscriber::fmt::init();
+    env_logger::Builder::from_env(Env::default().default_filter_or("infor")).init();
+
     let configuration = get_configuration().expect("Failed to read configuration file");
     let address = format!("127.0.0.1:{}", configuration.application_port);
     let listener = TcpListener::bind(address).expect("Failed to bind random port");
--- src/startup.rs
+++ src/startup.rs
@@ -1,5 +1,5 @@
 use crate::routes::{health_check, subscribe};
-use actix_web::{App, HttpServer, dev::Server, web};
+use actix_web::{App, HttpServer, dev::Server, middleware::Logger, web};
 use sqlx::PgPool;
 use std::net::TcpListener;
 
@@ -7,6 +7,7 @@ pub fn run(listener: TcpListener, connection: PgPool) -> Result<Server, std::io:
     let connection = web::Data::new(connection);
     let server = HttpServer::new(move || {
         App::new()
+            .wrap(Logger::default())
             .route("/health_check", web::get().to(health_check))
             .route("/subscriptions", web::post().to(subscribe))
             .app_data(connection.clone())
