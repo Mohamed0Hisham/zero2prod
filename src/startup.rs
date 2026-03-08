f57389bbaa4d56fec3b5aaa1144ddfa955d5d48c
mohamed hisham mahrous <hishamsovereign321@gmail.com>
2026-03-08 16:27:57 +0200

    fix: bug of duplicate request_id at tracing

--- src/startup.rs
+++ src/startup.rs
@@ -1,13 +1,14 @@
 use crate::routes::{health_check, subscribe};
-use actix_web::{App, HttpServer, dev::Server, middleware::Logger, web};
+use actix_web::{App, HttpServer, dev::Server, web};
 use sqlx::PgPool;
+use tracing_actix_web::TracingLogger;
 use std::net::TcpListener;
 
 pub fn run(listener: TcpListener, connection: PgPool) -> Result<Server, std::io::Error> {
     let connection = web::Data::new(connection);
     let server = HttpServer::new(move || {
         App::new()
-            .wrap(Logger::default())
+            .wrap(TracingLogger::default())
             .route("/health_check", web::get().to(health_check))
             .route("/subscriptions", web::post().to(subscribe))
             .app_data(connection.clone())
