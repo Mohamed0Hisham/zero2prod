1a8c25e29ad42646ac8f3bffbd516d6f78934c62
mohamed hisham mahrous <hishamsovereign321@gmail.com>
2026-03-08 17:34:15 +0200

    feat: introduce APP-ENVIRONMENT variable to work in production setting

--- src/configuration.rs
+++ src/configuration.rs
@@ -3,7 +3,7 @@ use secrecy::{ExposeSecret, SecretString};
 #[derive(serde::Deserialize)]
 pub struct Settings {
     pub database: DatabaseSettings,
-    pub application_port: u16,
+    pub application: ApplicationSettings,
 }
 
 #[derive(serde::Deserialize)]
@@ -17,23 +17,84 @@ pub struct DatabaseSettings {
 
 impl DatabaseSettings {
     pub fn connection_string(&self) -> SecretString {
-        SecretString::new(format!(
-            "postgres://{}:{}@{}:{}/{}",
-            self.username, self.password.expose_secret(), self.host, self.port, self.database_name
-        ).into())
+        SecretString::new(
+            format!(
+                "postgres://{}:{}@{}:{}/{}",
+                self.username,
+                self.password.expose_secret(),
+                self.host,
+                self.port,
+                self.database_name
+            )
+            .into(),
+        )
     }
     pub fn connection_string_without_db(&self) -> SecretString {
-        SecretString::new(format!(
-            "postgres://{}:{}@{}:{}",
-            self.username, self.password.expose_secret(), self.host, self.port
-        ).into())
+        SecretString::new(
+            format!(
+                "postgres://{}:{}@{}:{}",
+                self.username,
+                self.password.expose_secret(),
+                self.host,
+                self.port
+            )
+            .into(),
+        )
+    }
+}
+#[derive(serde::Deserialize)]
+pub struct ApplicationSettings {
+    pub port: u16,
+    pub host: String,
+}
+
+pub enum Environment {
+    Local,
+    Production,
+}
+
+impl Environment {
+    pub fn as_str(&self) -> &'static str {
+        match self {
+            Environment::Local => "local",
+            Environment::Production => "production",
+        }
+    }
+}
+impl TryFrom<String> for Environment {
+    type Error = String;
+    fn try_from(s: String) -> Result<Self, Self::Error> {
+        match s.to_lowercase().as_str() {
+            "local" => Ok(Self::Local),
+            "production" => Ok(Self::Production),
+            other => Err(format!(
+                "{} is not a supported environment. Use either `local` or `production`.",
+                other
+            )),
+        }
     }
 }
 
 pub fn get_configuration() -> Result<Settings, config::ConfigError> {
+    let base_path = std::env::current_dir().expect("Failed to determine the current directory");
+    let configuration_directory = base_path.join("configuration");
+
+    // Detect the running environment
+    let environment: Environment = std::env::var("APP_ENVIRONMENT")
+        .unwrap_or_else(|_| "local".into())
+        .try_into()
+        .expect("Failed to parse APP_ENVIRONMENT.");
+
     let settings = config::Config::builder()
-        .add_source(config::File::with_name("configuration"))
-        .build()?;
+        // Add the base configuration file
+        .add_source(config::File::from(configuration_directory.join("base")).required(true))
+        // Add the environment-specific file (e.g., local.yaml or production.yaml)
+        .add_source(
+            config::File::from(configuration_directory.join(environment.as_str())).required(true),
+        )
+        // Add settings from environment variables (with a prefix like APP_DATABASE__PORT)
+        .add_source(config::Environment::with_prefix("APP").separator("__"))
+        .build()?; // Finalize the configuration
 
     settings.try_deserialize::<Settings>()
 }
--- src/main.rs
+++ src/main.rs
@@ -5,15 +5,13 @@ use zero2prod::configuration::get_configuration;
 use zero2prod::startup::run;
 use zero2prod::telemetry::{get_subscriber, init_subscriber};
 
-fn create_listener(port: &u16) -> TcpListener {
-    let address = format!("127.0.0.1:{}", port);
+fn create_listener(host: String, port: u16) -> TcpListener {
+    let address = format!("{}:{}", host, port);
     TcpListener::bind(&address).expect("Failed to bind to address")
 }
 
-async fn create_db_pool(connection_string: &str) -> PgPool {
-    PgPool::connect(connection_string)
-        .await
-        .expect("Failed to connect to the database")
+fn create_db_pool(connection_string: &str) -> PgPool {
+    PgPool::connect_lazy(connection_string).expect("Failed to connect to the database")
 }
 
 #[tokio::main]
@@ -22,8 +20,11 @@ async fn main() -> std::io::Result<()> {
     init_subscriber(subscriber);
 
     let configuration = get_configuration().expect("Failed to read configuration file");
-    let listener = create_listener(&configuration.application_port);
-    let db_pool = create_db_pool(&configuration.database.connection_string().expose_secret()).await;
+    let listener = create_listener(
+        configuration.application.host,
+        configuration.application.port,
+    );
+    let db_pool = create_db_pool(&configuration.database.connection_string().expose_secret());
 
     run(listener, db_pool)?.await
 }
