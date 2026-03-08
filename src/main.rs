aa3ad2d401ce6f4c5614c73a609bdc50b64310c1
mohamed hisham mahrous <hishamsovereign321@gmail.com>
2026-03-08 15:27:51 +0200

    feat: subscriber can make logs sink

--- src/main.rs
+++ src/main.rs
@@ -17,7 +17,7 @@ async fn create_db_pool(connection_string: &str) -> PgPool {
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    let subscriber = get_subscriber("zero2prod".into(), "info".into());
+    let subscriber = get_subscriber("zero2prod".into(), "info".into(), std::io::stdout);
     init_subscriber(subscriber);
 
     let configuration = get_configuration().expect("Failed to read configuration file");
