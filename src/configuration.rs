4655881e7e48dd4484fd801f82d2f923aabd8a5e
mohamed hisham mahrous <hishamsovereign321@gmail.com>
2026-03-09 16:12:14 +0200

    fix: missing require_ssl option for db config

--- src/configuration.rs
+++ src/configuration.rs
@@ -1,6 +1,9 @@
 use secrecy::{ExposeSecret, SecretString};
 use serde_aux::field_attributes::deserialize_number_from_string;
-use sqlx::{ConnectOptions, postgres::PgConnectOptions};
+use sqlx::{
+    ConnectOptions,
+    postgres::{PgConnectOptions, PgSslMode},
+};
 
 #[derive(serde::Deserialize)]
 pub struct Settings {
@@ -16,20 +19,28 @@ pub struct DatabaseSettings {
     pub port: u16,
     pub host: String,
     pub database_name: String,
+    pub require_ssl: bool,
 }
 
 impl DatabaseSettings {
+    pub fn without_db(&self) -> PgConnectOptions {
+        let ssl_mode = if self.require_ssl {
+            PgSslMode::Require
+        } else {
+            //Try an encrypted connection, fallback to unencrypted if it fails
+            PgSslMode::Prefer
+        };
+        PgConnectOptions::new()
+            .host(&self.host)
+            .username(&self.username)
+            .password(self.password.expose_secret())
+            .port(self.port)
+            .ssl_mode(ssl_mode)
+    }
     pub fn with_db(&self) -> PgConnectOptions {
         self.without_db()
             .database(&self.database_name)
             .log_statements(tracing::log::LevelFilter::Trace)
-    }
-    pub fn without_db(&self) -> PgConnectOptions {
-        PgConnectOptions::new()
-            .host(&self.host)
-            .username(&self.username)
-            .password(&self.password.expose_secret())
-            .port(self.port)
     }
 }
 #[derive(serde::Deserialize)]
